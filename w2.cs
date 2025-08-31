06f8400a1cc881a5def578138c5e8bfc48ac9b2c
zanderesteban61-boop <zanderesteban61@gmail.com>
2025-08-31 08:19:49 +0800

    creating windows form

---
+++ w2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Queue
+{
+    public partial class w2 : Form
+    {
+        public w2()
+        {
+            InitializeComponent();
+        }
+    }
+}
